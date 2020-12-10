448ad9b84d3892e2e8dd0e50fdc0d0244cc1b2f1
dinhchanhnghia2409 <dinhchanhnghia2409@gmail.com>
2020-12-11 00:33:30 +0700

    add cloudinary

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('./middleware/requireLogin')
 const UserRouter = require('./Routes/User.route')
 const ProfileRouter = require('./Routes/Profile.route')
 const PostRouter = require('./Routes/Post.route')
-
+const ServerImage = require('./image_server/cloudinary')
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -16,6 +16,7 @@ require('./database')
 
 app.use(UserRouter);
 app.use(ProfileRouter);
+app.use(PostRouter);
 app.use(PostRouter);
 
 app.listen(PORT, () => {
