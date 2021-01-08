7209fa0f47bf414850b9b8d3ce71121ff8e6780a
dinhchanhnghia2409 <dinhchanhnghia2409@gmail.com>
2021-01-08 22:55:31 +0700

    a

--- Routes/Image.route.js
+++ Routes/Image.route.js
@@ -34,43 +34,6 @@ router.post('/uploadimage', async (req, res, next) => {
     })
 })
 
-const multer = require('multer')
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, 'images/')
-  },
-  filename: function(req, file, cb) {
-    console.log(file)
-    cb(null, file.originalname)
-  }
-})
 
-router.post('/upload', (req, res, next) => {
-    const upload = multer({ storage }).single('image')
-    upload(req, res, function(err) {
-      if (err) {
-        return res.send(err)
-      }
-      console.log('file uploaded to server')
-      console.log(req.file)
-        
-      const path = req.file.path
-      const uniqueFilename = new Date().toISOString()
-  
-      cloudinary.uploader.upload(
-        path,
-        { public_id:uniqueFilename, tags: 'nghia' }, // directory and tags are optional
-        function(err, image) {
-          if (err) return res.send(err)
-          console.log('Hình lên rồi Nghĩa ơi')
-          // remove file from server
-          const fs = require('fs')
-          fs.unlinkSync(path)
-          // return image details
-          res.json(image)
-        }
-      )
-    })
-  })
 
 module.exports = router;
