a5b5ad1542e10c1067a76406257189394af1f3bc
dinhchanhnghia2409 <dinhchanhnghia2409@gmail.com>
2021-01-10 12:13:04 +0700

    uploadbase64

--- Routes/Image.route.js
+++ Routes/Image.route.js
@@ -34,6 +34,17 @@ router.post('/uploadimage', async (req, res, next) => {
     })
 })
 
+//This route will upload file to cloudinary using base64
+router.post('/uploadimagebase64',RequireLogin, async (req, res, next) => {
+    var dataURI = req.body.dataURI;
+    console.log(dataURI)
+    var uploadStr = 'data:image/jpeg;base64,' + dataURI;
+
+    cloudinary.v2.uploader.upload(uploadStr,function(error, result)
+    {console.log(error,result); 
+    });
+})
+
 
 
 module.exports = router;
